86cac3ef541bc132ff5206b123e4abb009318d72
Tony A. Liu <tonyliu@New-Yorker.local>
2020-07-21 17:40:31 -0700

    .flaskenv

--- static/js/new.js
+++ static/js/new.js
@@ -6,6 +6,8 @@ class Worldmap {
     // Elements
     svg = null;
     g = null;
+    world = [];
+    names = [];
 
     // Configs
     svgW = 950;
@@ -106,38 +108,32 @@ class Worldmap {
 var files = ["/data/map.json", "/data/world-country-names.tsv"];
 
 Promise.all(files.map(url => d3.json(url))).then(function(values) {
-    console.log(values)
-});
+    vis.world = values [0]
+    vis.names = values [1]
+    // console.log(vis.world)
+      var globe = {type: "Sphere"},
+    land = topojson.feature(vis.world, vis.world.objects.land),
+    countries = topojson.feature(vis.world, vis.world.objects.countries).features,
+    borders = topojson.mesh(vis.world, vis.world.objects.countries, function(a, b) { return a !== b; });
+  // console.log(vis.world.objects.land)
 
-// queue()
-//     .defer(d3.json, "/data/map.json")
-//     .defer(d3.tsv, "/data/world-country-names.tsv")
-//     .await(ready);
-
-function ready(error, world, names) {
-  if (error) throw error;
-
-  var globe = {type: "Sphere"},
-    land = topojson.feature(world, world.objects.land),
-    countries = topojson.feature(world, world.objects.countries).features,
-    borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; });
-
-  countries = countries.filter(function(d) {
-    return names.some(function(n) {
-      if (d.id == n.id) return d.name = n.name;
-    });
-  }).sort(function(a, b) {
-    return a.name.localeCompare(b.name);
-  });
+  // countries = countries.filter(function(d) {
+  //   return vis.names.some(function(n) {
+  //     if (d.id == n.id) return d.name = n.name;
+  //   });
+  // }).sort(function(a, b) {
+  //   return a.name.localeCompare(b.name);
+  // });
 
   vis.map.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
+      .datum(topojson.feature(vis.world, vis.world.objects.land))
       .attr("class", "land")
       .attr("d", vis.path);
 
-  for(i = 0; i < names.length; i++) {
-    for (j = 0; j < countries.length; j++) {
-      if (countries[j].id == names[i].id) {
+  console.log()
+  for(let i = 0; i < vis.names.length; i++) {
+    for (let j = 0; j < countries.length; j++) {
+      if (countries[j].id === vis.names[i].id) {
         vis.map.insert("path", ".graticule")
           .datum(countries[j])
           .attr("fill", vis.colors.clickable)
@@ -185,11 +181,24 @@ function ready(error, world, names) {
     }
   }
     vis.map.insert("path", ".graticule")
-        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .datum(topojson.mesh(vis.world, vis.world.objects.countries, function(a, b) { return a !== b; }))
         .attr("class", "boundary")
         .attr("d", vis.path);
 
-};
+    // return (vis.world, vis.names)
+
+});
+// queue()
+//     .defer(d3.json, "/data/map.json")
+//     .defer(d3.tsv, "/data/world-country-names.tsv")
+//     .await(ready);
+
+// function ready(error, world, names) {
+//   if (error) throw error;
+
+
+
+// };
 
 d3.select(self.frameElement).style("height", vis.svgH + "px");
 
