65d4e2cdda9374d7bc8111bd007e8e13906f6e8f
Tony A. Liu <tonyliu@New-Yorker.local>
2020-07-24 12:31:26 -0700

    Interactive Progress and Button-v2

---
+++ static/js/index.js
@@ -0,0 +1,39 @@
+function displayWorld(){
+    document.getElementById("stage0").innerHTML = "world";
+    document.getElementById("stage1").innerHTML = "world";
+    document.getElementById("stage2").innerHTML = "world";
+    document.getElementById("stage3").innerHTML = "world";
+    document.getElementById("stage4").innerHTML = "world";
+}
+
+function displayNorthAmerica(){
+    document.getElementById("stage0").innerHTML = "NA";
+    document.getElementById("stage1").innerHTML = "NA";
+    document.getElementById("stage2").innerHTML = "NA";
+    document.getElementById("stage3").innerHTML = "NA";
+    document.getElementById("stage4").innerHTML = "NA";
+}
+
+function displayEurope(){
+    document.getElementById("stage0").innerHTML = "Europe";
+    document.getElementById("stage1").innerHTML = "NA";
+    document.getElementById("stage2").innerHTML = "NA";
+    document.getElementById("stage3").innerHTML = "NA";
+    document.getElementById("stage4").innerHTML = "NA";
+}
+
+function displayAsia(){
+    document.getElementById("stage0").innerHTML = "Asia";
+    document.getElementById("stage1").innerHTML = "NA";
+    document.getElementById("stage2").innerHTML = "NA";
+    document.getElementById("stage3").innerHTML = "NA";
+    document.getElementById("stage4").innerHTML = "NA";
+}
+
+function displayOceania(){
+    document.getElementById("stage0").innerHTML = "Oceania";
+    document.getElementById("stage1").innerHTML = "NA";
+    document.getElementById("stage2").innerHTML = "NA";
+    document.getElementById("stage3").innerHTML = "NA";
+    document.getElementById("stage4").innerHTML = "NA";
+}
