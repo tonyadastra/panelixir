f1e8dc47df2c6ecf93d41783b208dc3543b6d92b
Tony A. Liu <tonyliu@New-Yorker.local>
2020-07-26 17:37:55 -0700

    Update ReadeMe and Simplify Code(bars.js is now redundant)

--- static/js/main.js
+++ static/js/main.js
@@ -17,13 +17,13 @@ d3.json('/load_data').then(d => {
 
 }).catch(err => console.log(err));
 
-d3.json('/get_bars_data').then(d => {
-
-    // Redefine "data"
-    bars_data = d.bars_data;
-
-    // Instantiate Graph
-    bars = new Bars(bars_data, 'vis2')
-
-}).catch(err => console.log(err));
+// d3.json('/get_bars_data').then(d => {
+//
+//     // Redefine "data"
+//     bars_data = d.bars_data;
+//
+//     // Instantiate Graph
+//     bars = new Bars(bars_data, 'vis2')
+//
+// }).catch(err => console.log(err));
 
