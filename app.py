ce99cc7391d31c481ea11f7d2f66206e1c3844a4
lola <xul078@ucsd.edu>
2020-07-27 19:51:28 -0700

    move map to index.js

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import random, string, re
+import random
+import string
+import re
 from flask import Flask, render_template, request, jsonify
 from models.vaccine_info import Db, Vaccine
 import psycopg2
@@ -15,14 +17,14 @@ app = Flask(__name__)
 # Unquote following line to run at local
 
 # # User - Tony
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/vaccinedb'
-app.secret_key = "ILoveNewYork"
-conn = psycopg2.connect("dbname=vaccinedb user=postgres")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/vaccinedb'
+# app.secret_key = "ILoveNewYork"
+# conn = psycopg2.connect("dbname=vaccinedb user=postgres")
 
 # # User - Lola
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///covid19_db'
-# app.secret_key = "lola980109"
-# conn = psycopg2.connect("dbname=covid19_db user=lola")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///covid19_db'
+app.secret_key = "lola980109"
+conn = psycopg2.connect("dbname=covid19_db user=lola")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Db.init_app(app)
@@ -172,10 +174,12 @@ def getBarsData():
                 "GROUP BY stage, co_name, partner_name ORDER BY stage DESC, co_name, partner_name LIMIT 5;")
     bars_data = cur.fetchall()
     cur.execute("rollback")
+
+    # bar chart
     cur.execute("SELECT stage, COUNT(stage) as count "
-                " FROM info "
-                " WHERE continent LIKE '%" + continent + "%' "
-                                                         "GROUP BY stage ORDER BY stage")
+                "FROM info "
+                "WHERE continent LIKE '%" + continent + "%' "
+                "GROUP BY stage ORDER BY stage")
     continent_data = np.array(cur.fetchall(), dtype=object)
     cur.execute("rollback")
     data_arr = []
@@ -191,7 +195,16 @@ def getBarsData():
     for i in range(len(bars_data)):
         bars_data_json['bars_data'].append(bars_data[i][0][0])
     # print(json.dumps({'count': data_arr, 'bars_data': bars_data_json}))
-    return json.dumps({'count': data_arr, 'bars_data': bars_data_json})
+
+    # map
+    vaccines_json = {'vaccines': []}
+    vaccines = Vaccine.query.all()
+    for vaccine in vaccines:
+        vaccine_info = vaccine.__dict__
+        del vaccine_info['_sa_instance_state']
+        vaccines_json['vaccines'].append(vaccine_info)
+
+    return json.dumps({'count': data_arr, 'bars_data': bars_data_json, 'map_data': vaccines_json})
 
 
 @app.route('/load_data', methods=['GET'])
@@ -202,7 +215,7 @@ def load_data():
         vaccine_info = vaccine.__dict__
         del vaccine_info['_sa_instance_state']
         vaccines_json['vaccines'].append(vaccine_info)
-    return jsonify(vaccines_json)
+    return json.dumps({'map_data': vaccines_json})
 
 
 @app.route('/data/map.json', methods=['GET'])
@@ -212,17 +225,17 @@ def load_string():
     return jsonify(data)
 
 
-@app.route('/data/custom_geo.json', methods=['GET'])
-def load_map():
-    with open('data/custom_geo.json') as json_file:
-        data = json.load(json_file)
-    return jsonify(data)
+# @app.route('/data/custom_geo.json', methods=['GET'])
+# def load_map():
+#     with open('data/custom_geo.json') as json_file:
+#         data = json.load(json_file)
+#     return jsonify(data)
 
 
 @app.route('/data/world-country-names.tsv', methods=['GET'])
 def load_country():
     data = {}
-    with open('data/world-country-names.csv') as csvFile:
+    with open('data/WorldCountries.csv') as csvFile:
         csvReader = csv.DictReader(csvFile)
         for rows in csvReader:
             id = rows['id']
