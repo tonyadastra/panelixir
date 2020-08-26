17d835884a63acebaaa3acea51b51e78c8d92a19
Tony A. Liu <tonyliunyc@hotmail.com>
2020-08-26 16:42:31 -0700

    News Outline

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import csv
 application = app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:iloveNYC0704@panelixirdb.cxpzv5isdmqi.us-west-1.rds.amazonaws.com/vaccinedb'
 app.secret_key = "ILoveNewYork"
-conn = psycopg2.connect("host=panelixirdb.cxpzv5isdmqi.us-west-1.rds.amazonaws.com dbname=vaccinedb user=postgres password=iloveNYC0704")
-# conn = psycopg2.connect("dbname=vaccinedb user=postgres")
+# conn = psycopg2.connect("host=panelixirdb.cxpzv5isdmqi.us-west-1.rds.amazonaws.com dbname=vaccinedb user=postgres password=iloveNYC0704")
+conn = psycopg2.connect("dbname=vaccinedb user=postgres")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Db.init_app(app)
 cur = conn.cursor()
