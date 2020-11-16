900d5d8b69734666cb66a931495d1ccecc2b379c
Tony A. Liu <tonyliunyc@hotmail.com>
2020-11-15 18:17:38 -0800

    Handle Added Intro

--- auto_update_nytimes.py
+++ auto_update_nytimes.py
@@ -7,7 +7,6 @@ from models.format_nytimes_intro import format_intro
 import datetime
 import json
 
-
 id_response = ""
 response = ""
 now = datetime.datetime.now()
@@ -157,7 +156,7 @@ cur.execute(
                                 update_time date);''')
 for j in range(0, len(latest_update_array)):
     cur.execute('''INSERT INTO news_nytimes(vac_id, news_text, news_company, update_time)
-    VALUES (%s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'))''',
+        VALUES (%s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'))''',
                 (latest_update_array[j][3],
                  latest_update_array[j][0],
                  latest_update_array[j][1],
@@ -179,7 +178,7 @@ for i in range(len(latest_update_array)):
                 VaccineID = latest_update_array[i - j][3]
 
                 cur.execute('''INSERT INTO news(key, vac_id, tag, company, news_text, date)
-                VALUES (DEFAULT, %s, %s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'))''',
+                    VALUES (DEFAULT, %s, %s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'))''',
                             (VaccineID,
                              "New",
                              latest_update_array[i - j][1],
@@ -239,6 +238,8 @@ update_is_early_count = 0
 update_is_paused_count = 0
 update_is_combined_count = 0
 update_intro_count = 0
+new_companies_added = 0
+new_assigned_id_count = 0
 # Find all Phase III intro
 phase2_and_3_company_intro = soup.find_all('p', attrs={
     "class": "g-body g-list-item g-filter-item g-filter-phase2 g-filter-phase3"})
@@ -265,7 +266,6 @@ all_preclinical_intro = soup.find_all('p', attrs={"class": "g-body g-list-item g
 phase0_count = len(all_preclinical_intro)
 
 all_vaccines_intro = all_phase3_intro + all_phase2_intro + all_phase1_company_intro + all_preclinical_intro
-
 
 new_data_array = []
 for intro in all_vaccines_intro:
@@ -421,30 +421,31 @@ cur.execute("SELECT * from nytimes ORDER BY intro_id;")
 existing_data_array = cur.fetchall()
 cur.execute("rollback")
 
-if len(existing_data_array) == len(new_data_array):
-    for i in range(len(new_data_array)):
-        new_vaccine_id = new_data_array[i][0]
-        new_stage = new_data_array[i][1]
-        new_company_name = new_data_array[i][2]
-        new_vaccine_intro = new_data_array[i][3]
-        new_date = new_data_array[i][4]
-        new_is_combined_phases = str(new_data_array[i][5])
-        new_is_early = str(new_data_array[i][6])
-        new_is_paused = str(new_data_array[i][7])
+for i in range(len(new_data_array)):
+    new_vaccine_id = new_data_array[i][0]
+    new_stage = new_data_array[i][1]
+    new_company_name = new_data_array[i][2]
+    new_vaccine_intro = new_data_array[i][3]
+    new_date = new_data_array[i][4]
+    new_is_combined_phases = str(new_data_array[i][5])
+    new_is_early = str(new_data_array[i][6])
+    new_is_paused = str(new_data_array[i][7])
 
-        old_vaccine_id = existing_data_array[i][0]
-        old_stage = existing_data_array[i][1]
-        old_company_name = existing_data_array[i][2]
-        old_vaccine_intro = existing_data_array[i][3]
-        old_date = existing_data_array[i][4]
-        old_is_combined_phases = str(existing_data_array[i][5])
-        old_is_early = str(existing_data_array[i][6])
-        old_is_paused = str(existing_data_array[i][7])
-        proceed = False
-        # Add check
-        if new_vaccine_id == old_vaccine_id:
-            proceed = True
+    old_vaccine_id = existing_data_array[i][0]
+    old_stage = existing_data_array[i][1]
+    old_company_name = existing_data_array[i][2]
+    old_vaccine_intro = existing_data_array[i][3]
+    old_date = existing_data_array[i][4]
+    old_is_combined_phases = str(existing_data_array[i][5])
+    old_is_early = str(existing_data_array[i][6])
+    old_is_paused = str(existing_data_array[i][7])
+    # if len(existing_data_array) == len(new_data_array):
+    proceed = False
+    # Add check
+    if new_vaccine_id == old_vaccine_id:
+        proceed = True
 
+    if proceed:
         try:
             cur.execute("SELECT TO_DATE(%s, 'Mon FMDD YYYY') - %s AS interval, "
                         "TO_DATE(%s, 'Mon FMDD YYYY') - update_date AS info_interval, "
@@ -463,72 +464,66 @@ if len(existing_data_array) == len(new_data_array):
 
         # If update in date: interval!=0(not None)-->update; original None + new NOT None-->update
         if (date_interval != 0 and date_interval is not None) or (old_date is None and new_date != ''):
-            if proceed:
-                # update date
-                cur.execute("UPDATE nytimes SET date = TO_DATE(%s, 'Mon FMDD YYYY') WHERE vac_id = %s",
+            # update date
+            cur.execute("UPDATE nytimes SET date = TO_DATE(%s, 'Mon FMDD YYYY') WHERE vac_id = %s",
+                        (new_date, new_vaccine_id))
+            conn.commit()
+
+            # If new date from NYTimes is older than info date, abort the update
+            if interval_info is not None and interval_info >= 0 and allow_auto_update:
+                cur.execute("UPDATE info SET update_date = TO_DATE(%s, 'Mon FMDD YYYY') WHERE info.vac_id = %s",
                             (new_date, new_vaccine_id))
                 conn.commit()
-
-                # If new date from NYTimes is older than info date, abort the update
-                if interval_info is not None and interval_info >= 0 and allow_auto_update:
-                    cur.execute("UPDATE info SET update_date = TO_DATE(%s, 'Mon FMDD YYYY') WHERE info.vac_id = %s",
-                                (new_date, new_vaccine_id))
-                    conn.commit()
-                    update_date_count += 1
+                update_date_count += 1
 
         # update stage
         if new_stage != old_stage:
-            if proceed:
-                cur.execute("UPDATE nytimes SET stage = %s WHERE vac_id = %s", (new_stage, new_vaccine_id))
-                conn.commit()
+            cur.execute("UPDATE nytimes SET stage = %s WHERE vac_id = %s", (new_stage, new_vaccine_id))
+            conn.commit()
 
-                if allow_auto_update:
-                    cur.execute("UPDATE info SET stage = %s WHERE vac_id = %s", (new_stage, new_vaccine_id))
-                    conn.commit()
-                    update_stage_count += 1
+            if allow_auto_update:
+                cur.execute("UPDATE info SET stage = %s WHERE vac_id = %s", (new_stage, new_vaccine_id))
+                conn.commit()
+                update_stage_count += 1
 
         # update combined phases
         if new_is_combined_phases != old_is_combined_phases:
-            if proceed:
-                # Update nytimes
-                cur.execute("UPDATE nytimes SET combined_phases = %s WHERE vac_id = %s",
+            # Update nytimes
+            cur.execute("UPDATE nytimes SET combined_phases = %s WHERE vac_id = %s",
+                        (new_is_combined_phases, new_vaccine_id))
+            conn.commit()
+
+            if allow_auto_update:
+                cur.execute("UPDATE info SET combined_phases = %s WHERE vac_id = %s",
                             (new_is_combined_phases, new_vaccine_id))
                 conn.commit()
-
-                if allow_auto_update:
-                    cur.execute("UPDATE info SET combined_phases = %s WHERE vac_id = %s",
-                                (new_is_combined_phases, new_vaccine_id))
-                    conn.commit()
-                    update_is_combined_count += 1
+                update_is_combined_count += 1
 
         # update early
         if new_is_early != old_is_early:
-            if proceed:
-                # Update nytimes
-                cur.execute("UPDATE nytimes SET early_approval = %s WHERE vac_id = %s",
+            # Update nytimes
+            cur.execute("UPDATE nytimes SET early_approval = %s WHERE vac_id = %s",
+                        (new_is_early, new_vaccine_id))
+            conn.commit()
+
+            if allow_auto_update:
+                cur.execute("UPDATE info SET early_approval = %s WHERE vac_id = %s",
                             (new_is_early, new_vaccine_id))
                 conn.commit()
-
-                if allow_auto_update:
-                    cur.execute("UPDATE info SET early_approval = %s WHERE vac_id = %s",
-                                (new_is_early, new_vaccine_id))
-                    conn.commit()
-                    update_is_early_count += 1
+                update_is_early_count += 1
 
         # update paused
         if new_is_paused != old_is_paused:
-            if proceed:
-                # Update nytimes
-                cur.execute("UPDATE nytimes SET paused = %s WHERE vac_id = %s", (new_is_paused, new_vaccine_id))
-                conn.commit()
+            # Update nytimes
+            cur.execute("UPDATE nytimes SET paused = %s WHERE vac_id = %s", (new_is_paused, new_vaccine_id))
+            conn.commit()
 
-                if allow_auto_update:
-                    cur.execute("UPDATE info SET paused = %s WHERE vac_id = %s", (new_is_paused, new_vaccine_id))
-                    conn.commit()
-                    update_is_paused_count += 1
+            if allow_auto_update:
+                cur.execute("UPDATE info SET paused = %s WHERE vac_id = %s", (new_is_paused, new_vaccine_id))
+                conn.commit()
+                update_is_paused_count += 1
 
         # If there is an update in vaccine intro
-        # if new_vaccine_intro != old_vaccine_intro:
         new_intro_array = new_vaccine_intro.split('. ')
         old_intro_array = old_vaccine_intro.split('. ')
 
@@ -581,6 +576,155 @@ if len(existing_data_array) == len(new_data_array):
             conn.commit()
             update_intro_count += 1
 
+    else:
+        if old_company_name != new_company_name:
+            if new_vaccine_id == -1:
+                cur.execute("SELECT vac_id, company FROM info")
+                alternate_info_id_and_company = cur.fetchall()
+                cur.execute("rollback")
+                info_company_array_possibilities = []
+                # Update data format - transform to 1d array
+                for j in range(len(alternate_info_id_and_company)):
+                    info_company_array_possibilities.append(alternate_info_id_and_company[j][1])
+
+                index = get_close_matches_indexes(new_company_name, info_company_array_possibilities, n=1, cutoff=0.7)
+                try:
+                    new_vaccine_id = alternate_info_id_and_company[index[0]][0]
+                except IndexError:
+                    new_vaccine_id = -1
+                    # Find match for vaccines with multiple companies but only one shows in news
+                    # Problem: possible repeats in companies!
+                    for a in range(len(alternate_info_id_and_company)):
+                        each_id_company = info_company_array_possibilities[a].split(', ')
+                        match = difflib.get_close_matches(new_company_name, each_id_company, n=1, cutoff=1.0)
+                        if match:
+                            new_vaccine_id = alternate_info_id_and_company[a][0]
+                            # intro_id_response += "Found match for news #" + str(idx + 1) + \
+                            #                " in try-except-1. Paired with VaccineID " + str(new_vaccine_id) + ".||"
+                            break
+                        else:
+                            new_vaccine_id = -1
+                    split_company_array = new_company_name.split(', ')
+                    # Find match for vaccines with multiple companies - rearrange the order in company_string
+                    # Iterate through every possible combination of company names to find match
+                    if new_vaccine_id == -1 and ", " in new_company_name:
+                        count = 0
+                        increment = 0
+                        while count < len(split_company_array) and new_vaccine_id == -1:
+                            order_start = 1 + increment
+                            order_count = 0
+                            if order_start >= len(split_company_array):
+                                order_start = 0
+                            new_company_string = ""
+                            while order_count < len(split_company_array):
+                                if order_count == len(split_company_array) - 1:
+                                    new_company_string += split_company_array[order_start]
+                                else:
+                                    new_company_string += split_company_array[order_start] + ", "
+                                order_count += 1
+                                if order_start >= len(split_company_array) - 1:
+                                    order_start = 0
+                                else:
+                                    order_start += 1
+
+                            index = get_close_matches_indexes(new_company_string, info_company_array_possibilities, n=1,
+                                                              cutoff=0.6)
+                            try:
+                                new_vaccine_id = alternate_info_id_and_company[index[0]][0]
+                                # id_response += "Found match for news #" + str(idx + 1) + \
+                                #                " in try-except-2. Paired with VaccineID " + str(new_vaccine_id) + ".||"
+                            except IndexError:
+                                new_vaccine_id = -1
+                            increment += 1
+                            count += 1
+
+                    if new_vaccine_id == -1:
+                        modified_string = new_company_name + " Biological"
+                        index = get_close_matches_indexes(modified_string, info_company_array_possibilities, n=1,
+                                                          cutoff=0.7)
+                        try:
+                            new_vaccine_id = alternate_info_id_and_company[index[0]][0]
+                            # id_response += "Found match for news #" + str(idx + 1) + \
+                            #                " in try-except-3. Paired with VaccineID " + str(new_vaccine_id) + ".||"
+                        except IndexError:
+                            new_vaccine_id = -1
+
+            # If cannot find vaccine_id--> Assign New ID
+            if new_vaccine_id == -1:
+                cur.execute("SELECT vac_id FROM info ORDER BY vac_id DESC LIMIT 1")
+                new_assigned_id = cur.fetchone()[0] + 1
+                # Update NYTimes table
+                if new_date == '':
+                    cur.execute('''INSERT INTO nytimes(vac_id, stage, company_name, vaccine_intro, combined_phases, early_approval,
+                                                    paused, intro_id)
+                                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''',
+                                (
+                                    new_assigned_id, new_stage, new_company_name, new_vaccine_intro,
+                                    new_is_combined_phases,
+                                    new_is_early, new_is_paused, i))
+                    conn.commit()
+
+                else:
+                    cur.execute('''INSERT INTO nytimes(vac_id, stage, company_name, vaccine_intro, date, combined_phases,
+                                                    early_approval, paused, intro_id)
+                                                    VALUES (%s, %s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'), %s, %s, %s, %s)''',
+                                (new_assigned_id, new_stage, new_company_name, new_vaccine_intro, new_date,
+                                 new_is_combined_phases, new_is_early, new_is_paused, i))
+                    conn.commit()
+                # Update INFO table
+                cur.execute('''INSERT INTO info(vac_id, stage, company, intro, country, vac_type, 
+                                                    update_date, combined_phases, early_approval, paused)
+                                                    VALUES (%s, %s, %s, %s, %s, %s, CURRENT_DATE, %s, %s, %s)''',
+                            (new_assigned_id, new_stage, new_company_name, new_vaccine_intro, '', '',
+                             new_is_combined_phases, new_is_early, new_is_paused))
+                # Update COMPANIES table
+                cur.execute("INSERT INTO companies(vac_id, co_name, company_nytimes) VALUES (%s, %s, %s)",
+                            (new_assigned_id, new_company_name, new_company_name))
+                conn.commit()
+
+                new_assigned_id_count += 1
+            # If found vaccine_id --> update existing intro
+            else:
+                # Update NYTimes table
+                if new_date == '':
+                    cur.execute('''INSERT INTO nytimes(vac_id, stage, company_name, vaccine_intro, combined_phases, early_approval,
+                                                    paused, intro_id)
+                                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''',
+                                (
+                                    new_vaccine_id, new_stage, new_company_name, new_vaccine_intro,
+                                    new_is_combined_phases,
+                                    new_is_early, new_is_paused, i))
+                    conn.commit()
+
+                else:
+                    cur.execute('''INSERT INTO nytimes(vac_id, stage, company_name, vaccine_intro, date, combined_phases,
+                                                    early_approval, paused, intro_id)
+                                                    VALUES (%s, %s, %s, %s, TO_DATE(%s, 'Mon FMDD YYYY'), %s, %s, %s, %s)''',
+                                (new_vaccine_id, new_stage, new_company_name, new_vaccine_intro, new_date,
+                                 new_is_combined_phases, new_is_early, new_is_paused, i))
+                    conn.commit()
+                try:
+                    cur.execute("SELECT allow_auto_update "
+                                "FROM info "
+                                "WHERE vac_id = %s",
+                                (new_vaccine_id,))
+                    allow_auto_update = cur.fetchone()[0]
+                except TypeError:
+                    allow_auto_update = False
+                if allow_auto_update:
+                    cur.execute("SELECT intro FROM info WHERE vac_id = %s", (new_vaccine_id,))
+                    existing_intro = cur.fetchone()[0]
+                    if existing_intro is not None and existing_intro != '':
+                        updated_intro = existing_intro + "<br><br>" + new_vaccine_intro
+                    else:
+                        updated_intro = new_vaccine_intro
+                    cur.execute("UPDATE info SET stage = %s, update_date = CURRENT_DATE, intro = %s, "
+                                "combined_phases = %s, early_approval = %s, paused = %s",
+                                (new_stage, updated_intro, new_is_combined_phases, new_is_early, new_is_paused))
+
+            new_companies_added += 1
+
+
 return_response = {
     'news_update': {
         'statusCode': 200,
@@ -604,7 +748,11 @@ return_response = {
             'early_approval': update_is_early_count,
             'paused': update_is_paused_count,
             'intro': update_intro_count
-        }
+        },
+        'new_vaccines': {
+                'total_new_added': new_companies_added,
+                'number of vaccine_id assigned': new_assigned_id_count
+            }
 
     }
 }
