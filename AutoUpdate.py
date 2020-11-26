18f415d550bdaf4d992521d1eead3aaf2d970ca9
Tony A. Liu <tonyliunyc@hotmail.com>
2020-11-26 00:43:42 -0800

    v1.0 Release

---
+++ AutoUpdate.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import json
+import requests
+from bs4 import BeautifulSoup
+import psycopg2
+import datetime
+
+update_count = 0
+
+
+# def auto_update_google(event, context):
+# global update_count
+# connect to database
+# conn = psycopg2.connect("dbname=vaccinedb user=postgres")
+conn = psycopg2.connect("host=panelixirdb.cxpzv5isdmqi.us-west-1.rds.amazonaws.com"
+                        " dbname=vaccinedb user=postgres password=iloveNYC0704")
+cur = conn.cursor()
+# scrape results from the New York Times
+result = requests.get("https://www.google.com/search?tbm=nws&q=vaccine+news&oq=vaccine+news")
+if result.status_code == 200:
+    src = result.content
+    soup = BeautifulSoup(src, "html.parser")
+
+    # Find Latest News Section
+    cur.execute("SELECT news_text, source, link FROM news WHERE category = %s", ('G',))
+    existing_articles = cur.fetchall()
+    cur.execute("rollback")
+
+    google_news = soup.find_all('div', attrs={"class": "kCrYT"})
+
+    new_vaccine_articles = []
+    for news in google_news:
+        news_array = []
+        news_title = news.find_all('div', attrs={"class": "BNeawe vvjwJb AP7Wnd"})
+        if news_title:
+            source = news.find_all('div', attrs={"class": "BNeawe UPmit AP7Wnd"})
+            link = news.a['href'].replace('/url?q=', '').split('&sa=U')[0]
+            news_array.append(news_title[0].text)
+            news_array.append(source[0].text)
+            news_array.append(link)
+
+            new_vaccine_articles.append(news_array)
+            print(news_title)
+            print(source)
+            print(link)
+
+    print(new_vaccine_articles)
+    for i in range(len(new_vaccine_articles)):
+        new_text = new_vaccine_articles[i][0]
+        new_source = new_vaccine_articles[i][1]
+        new_link = new_vaccine_articles[i][2]
+        proceed = True
+        for j in range(len(existing_articles)):
+            old_text = existing_articles[j][0]
+            old_source = existing_articles[j][1]
+            old_link = existing_articles[j][2]
+            if new_text == old_text or new_link == old_link:
+                proceed = False
+        # TODO: Filter News from Google
+        discard_keywords = []
+        print(proceed)
+        if proceed:
+            cur.execute('''INSERT INTO news(tag, news_text, source, link, category) VALUES (%s, %s, %s, %s, %s)''',
+                        ("New", new_text, new_source, new_link, 'G'))
+            conn.commit()
+            update_count += 1
+
+return_response = {
+    'URLStatusCode': result.status_code,
+    'update_count': update_count
+
+}
+
+print(json.dumps(return_response))
+
+
+
+
---
+++ models/format_nytimes_intro.py
@@ -0,0 +1,68 @@
+""" @author Tony Liu, Lola Liu
+    @version v1.0.1 - November 19, 2020
+    @project PanElixir - Global COVID-19 Vaccine Tracker
+    @description This program will return NYTimes vaccine intro in appropriate format to determine whether an
+    update is present in the latest scraped data
+"""
+
+
+def format_intro(nytimes_intro):
+    join_indexes = []
+    for idx, intro in enumerate(nytimes_intro):
+        if intro[0].isdigit():
+            join_indexes.append(idx - 1)
+        join_keywords = ['U.S', 'St', 'U.K', 'U.A.E', 'F.D.A', 'Dr', 'Vladimir V', 'C.E.O']
+        for keyword in join_keywords:
+            if intro.endswith(keyword):
+                join_indexes.append(idx)
+
+    formatted_new_intro = []
+
+    class ContinueLoop(Exception):
+        pass
+
+    # if len(join_indexes) != 0:
+    for j in range(len(nytimes_intro)):
+        # for idx in join_indexes:
+        try:
+            for k in range(1, j + 1):
+                if j - k in join_indexes:
+                    raise ContinueLoop()
+                else:
+                    break
+        except ContinueLoop:
+            continue
+
+        if j in join_indexes:
+            string = nytimes_intro[j] + '. ' + nytimes_intro[j + 1]
+            for k in range(1, len(nytimes_intro) - j):
+                if j + k in join_indexes:
+                    string += '. ' + nytimes_intro[j + k + 1]
+                else:
+                    break
+            # if j + 2 in join_indexes:
+            #     string += '. ' + nytimes_intro[j + 3]
+            if not string.endswith('.') and not string.endswith('. '):
+                string += '. '
+            if string.endswith('.'):
+                string += ' '
+            formatted_new_intro.append(string)
+        else:
+            if not nytimes_intro[j].endswith('.') and not nytimes_intro[j].endswith('. '):
+                nytimes_intro[j] += '. '
+            if nytimes_intro[j].endswith('.'):
+                nytimes_intro[j] += ' '
+            formatted_new_intro.append(nytimes_intro[j])
+
+    intro_after_split_quote = []
+    for intro in formatted_new_intro:
+        if ".” " in intro:
+            split_quote_intro = intro.split(".” ")
+            for idx, new_intro in enumerate(split_quote_intro):
+                if idx == 0:
+                    new_intro += ".” "
+                intro_after_split_quote.append(new_intro)
+        else:
+            intro_after_split_quote.append(intro)
+
+    return intro_after_split_quote
